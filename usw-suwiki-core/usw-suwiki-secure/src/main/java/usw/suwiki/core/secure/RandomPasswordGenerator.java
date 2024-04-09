32d7e4c1c4ca99b2f816cfca03c21dd27f6dc97e
Hejow <gmlwh124@naver.com>
2024-04-09 09:32:06 +0900

    feat: random password 특수문자 반영

--- usw-suwiki-core/usw-suwiki-secure/src/main/java/usw/suwiki/core/secure/RandomPasswordGenerator.java
+++ usw-suwiki-core/usw-suwiki-secure/src/main/java/usw/suwiki/core/secure/RandomPasswordGenerator.java
@@ -9,18 +9,27 @@ import java.util.stream.Collectors;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RandomPasswordGenerator {
-  private static final int RANDOM_PASSWORD_SIZE = 8;
+  private static final int RANDOM_CHARACTER_SIZE = 7;
 
   private static final SecureRandom RANDOM = new SecureRandom();
-  private static final List<String> CHARACTERS_LIST = List.of(
-    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^",
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+
+  private static final List<String> SYMBOLS = List.of("!", "@", "#", "$", "%", "^");
+  private static final List<String> CHARACTERS = List.of(
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
   );
 
   public static String generate() {
-    return RANDOM.ints(RANDOM_PASSWORD_SIZE, 0, CHARACTERS_LIST.size())
-      .mapToObj(CHARACTERS_LIST::get)
+    var index = RANDOM.nextInt(RANDOM_CHARACTER_SIZE);
+    var randomSymbol = SYMBOLS.get(RANDOM.nextInt(SYMBOLS.size()));
+
+    var randomCharacters = RANDOM.ints(RANDOM_CHARACTER_SIZE, 0, CHARACTERS.size())
+      .mapToObj(CHARACTERS::get)
       .collect(Collectors.joining());
+
+    return randomCharacters.substring(0, index) + randomSymbol + randomCharacters.substring(index);
   }
 }
--- usw-suwiki-core/usw-suwiki-secure/src/test/java/usw/suwiki/core/secure/RandomPasswordGeneratorTest.java
+++ usw-suwiki-core/usw-suwiki-secure/src/test/java/usw/suwiki/core/secure/RandomPasswordGeneratorTest.java
@@ -7,9 +7,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class RandomPasswordGeneratorTest {
+  private static final String[] SYMBOLS = {"!", "@", "#", "$", "%", "^"};
 
   @RepeatedTest(10)
-  @DisplayName("랜덤한 비밀번호를 생성하면 8자리 이하여야 한다.")
+  @DisplayName("랜덤한 비밀번호를 생성하면 8자리 이하의 특수문자를 포함한 값이어야 한다.")
   void generate_Success_Within8Letters() {
     // given
 
@@ -19,7 +20,8 @@ class RandomPasswordGeneratorTest {
     // then
     assertAll(
       () -> assertThat(password).isNotBlank(),
-      () -> assertThat(password).hasSizeLessThanOrEqualTo(8)
+      () -> assertThat(password).hasSize(8),
+      () -> assertThat(password).containsAnyOf(SYMBOLS)
     );
   }
 }
